b34dd0e379b7bba9a78169b30df0d39570d9d107
dalcoj <jacqueline.dalcorn@ons.gov.uk>
2017-11-24 12:20:55 +0000

    adding country service wrapper

---
+++ _prototypes/typeahead/passport-type/js/CountryService.js
@@ -0,0 +1,12 @@
+/**
+ * Country Register service singleton
+ */
+window.countryRegisterService = (function () {
+	var baseUrl = 'https://country.register.gov.uk/records.json',
+  	loadJSON(url, typeaheadData); 
+
+  	function typeaheadData(data) {
+  		data.
+  	}
+
+});
