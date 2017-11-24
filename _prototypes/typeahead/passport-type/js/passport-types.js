39a404271e435238c52ed1727d9a457078e4b274
dalcoj <jacqueline.dalcorn@ons.gov.uk>
2017-11-24 12:28:16 +0000

    formatting

--- _prototypes/typeahead/passport-type/js/passport-types.js
+++ _prototypes/typeahead/passport-type/js/passport-types.js
@@ -16,20 +16,20 @@
 
 
 
-	// from records.json or https://country.register.gov.uk/
-	// pull data 
+	// data from https://country.register.gov.uk/records.json
 	// use "name" as options 
-	// "name" must link to "country" "official-name" and "citizen-names" 
+	// "name" must map to "country" "official-name" and "citizen-names" 
 	// allowing the "name" to appear when any of those values are entered 
-	// can we add in an "abbreviation" value? eg. USA
-	// can we add in an "translation" value? eg. Francais
+	
+	// can we add in an "abbreviation" value for each entry? eg. USA
+	// can we add in a "translation" value for each entry? eg. Francais
 
 	// Ireland and United Kingdom to appear as soon as focus is in input box 
 
 	// when country is selected 
 	// add playback__item 
 	// append {{name}} to ID of each playback__item eg. playback__item__united_kingdom
-	// if it is _1 then #all-passports-heading needs to appear at the same time
+	// if it is the first playback__item then #all-passports-heading needs to appear at the same time
 	// if more than one playback__item, the countries should appear A-Z 
 
 	// when remove button is pressed 
@@ -38,11 +38,11 @@
 
 
 	// non-JS version 
-	// dropdown 
+	// displays just as dropdown 
 	// using "name" as select options
 
 
-	// version 2 
+	// version 2 page 2
 	// if Ireland or UK are selected on first screen 
 	// pass through those values to appear in the playback 
 	// add a message above the typeahead "{{United Kingdom}} has already been selected" (Highlight the country name)
