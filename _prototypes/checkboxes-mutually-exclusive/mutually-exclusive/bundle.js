e23ab7a66a84b8adcccc958b0ca3528d78dccd33
Benn Pearson <bennpearson@gmail.com>
2018-03-06 16:40:49 +0000

    a11y amends on checkboxe js

--- _prototypes/checkboxes-mutually-exclusive/mutually-exclusive/bundle.js
+++ _prototypes/checkboxes-mutually-exclusive/mutually-exclusive/bundle.js
@@ -29,22 +29,23 @@
       let $otherCheckboxes = $('input:checkbox[name=' + nameGroup + ']:checked').not(this),
         selectedGroupIndex = getSelectedMutullyExclusiveGroupIndexForCheckbox($(this));
 
-      $(this).prop('aria-checked', true);
+      $(this).attr('aria-checked', true);
 
       if (selectedGroupIndex === undefined) {
         console.log('Checkbox not found in group: ', $(this));
       }
       else {
+      $('#input-alert').text("");
         $otherCheckboxes.each(function(i, checkboxEl) {
-
           setTimeout(function () {
             let $checkBox = $(checkboxEl),
               groupIndex = getSelectedMutullyExclusiveGroupIndexForCheckbox($checkBox);
 
             if (groupIndex !== selectedGroupIndex) {
-              $checkBox.prop('checked', false);
-              $checkBox.prop('aria-checked', false);
+              $checkBox.attr('checked', false);
+              $checkBox.attr('aria-checked', false);
               $checkBox.parent().removeClass('is-checked');
+              $('#input-alert').append(checkboxEl.value + " deselected. ");
             }
           }, 0);
         });
