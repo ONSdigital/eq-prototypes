21f45dc4f2b459a7d21b3617a4728af51defba8c
boxadesign <info@boxadesign.co.uk>
2020-03-12 15:17:11 +0000

    Added char check for job title field

--- _prototypes/individual-response--your-household-v15/bundle.js
+++ _prototypes/individual-response--your-household-v15/bundle.js
@@ -14,6 +14,7 @@ import './assets/lib/abortcontroller-polyfill';
  */
 import './assets/modules/typeahead-refactored/typeahead.module';
 import './assets/modules/uac/uac';
+import './assets/modules/character-check';
 
 import {
   RELATIONSHIPS_STORAGE_KEY,
