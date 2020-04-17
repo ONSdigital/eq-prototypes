1b20e09c07387627ce5903d1f5f81c442c97cffa
boxadesign <info@boxadesign.co.uk>
2020-04-17 08:52:31 +0100

    Fixed adding person_id to previous links

--- _prototypes/end-to-end/assets/previous-link.js
+++ _prototypes/end-to-end/assets/previous-link.js
@@ -2,6 +2,9 @@ import domready from '../../../_js/modules/domready';
 
 domready(() => {
   let previousURL;
+  const urlParams = new URLSearchParams(window.location.search);
+  const personID = urlParams.get('person_id');
+
   const pathName = window.location.pathname;
   const pageData = JSON.parse(sessionStorage.getItem('pageData'));
 
@@ -13,7 +16,7 @@ domready(() => {
     const previousLinks = [...document.querySelectorAll('.js-previous-link')];
 
     previousLinks.forEach(link => {
-      link.setAttribute('href', previousURL);
+      link.setAttribute('href', previousURL + (personID ? '?person_id=' + personID : ''));
     });
   }
 });
